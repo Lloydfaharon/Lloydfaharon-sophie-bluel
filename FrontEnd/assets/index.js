7922ead4a12dea573dd89b288ba5bc3107b9bc94
Lloydfaharon <143098047+Lloydfaharon@users.noreply.github.com>
2024-01-19 11:29:01 +0100

    maj19

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,6 +1,23 @@
-async function getWorks() {
-    const response = await fetch("http://localhost:5678/api-docs/");
-    const responseJson =  response.json();
-    console.log(responseJson);
-}
+
+//vérifier que son Jason server et bien enroute
+
+/***** variables *****/
 getWorks();
+//affichageWorks();
+
+
+const gallery = document.querySelector("gallery");
+
+async function getWorks() {
+    const response = await fetch("http://localhost:5678/api/works");
+    const responseJason=response.json()
+    console.log(responseJason)
+    //return await response.json();
+}
+
+
+//async function affichageWorks() {
+    //const arrayWorks = await getWorks();
+
+//}
+
