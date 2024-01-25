cf5d973987290edf7438894452c8fe998092e037
Lloydfaharon <143098047+Lloydfaharon@users.noreply.github.com>
2024-01-25 11:59:01 +0100

    login25

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,9 +1,59 @@
 // Les variable pour le login //
-const email = document.querySelector("form #email");
-const password = document.querySelector("form #password");
+
+//const { type } = require("os");
+
+const balisEmail = document.getElementById("email");
+const balisPassword = document.getElementById("password");
 const form =document.querySelector("form")
 const messageErreur = document.querySelector("#login p")
 
-forn.addEventlistener("submit",(event) =>{
-    event.preventDefault 
-})
+//async function getLogin(){
+   // const response = await fetch("http://localhost:5678/api/users/login")
+    //return await response.json();
+
+//}
+//const userIdentifiants =  fetch ("http://localhost:5678/api/users/login"){
+    //method: "POST" ,
+    //body: vierifierChanp,
+    //headers: { "content-type" :"application/json"}
+//}
+form.addEventListener("submit",(event) =>  {
+    event.preventDefault();
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+        email: element.email.value,
+        password: element.password.value,
+        }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            sessionStorage.setItem("Token", data.token);
+
+            if (data.message || data.error) {
+                alert("Erreur dans l\'identifiant ou le mot de passe");
+            } else {
+                sessionStorage.setItem("isConnected", JSON.stringify(true));
+                window.location.replace("index.html");
+            }
+        })
+});
+
+
+
+
+//getLogin()
+//form.addEventListener("submit",(event) => {
+   // event.preventDefault()
+    //vierifierChanp(balisEmail)
+   // let email= balisEmail.value
+   // console.log(email.value , password.value )
+    
+
+    
+//})
